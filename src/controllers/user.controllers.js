b1a85f35590992eeede1d7c190fcf6483409fa5c
Your Name <you@example.com>
2024-12-03 14:27:53 +0500

    login user and register user done

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -33,4 +33,72 @@ const generateRefreshToken = (user) => {
     expiresIn: "7d",
   });
 };
-export { encryptPassword, generateAccessToken, generateRefreshToken };
+
+const registerUser = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await Users.findOne({
+    email: email,
+  });
+  if (user) {
+    res.status(401).json({
+      message: "User already exist",
+    });
+  } else {
+    const createUser = Users.create({
+      email,
+      password,
+    });
+    res.status(200).json({
+      message: "user added successfully",
+    });
+  }
+};
+
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await Users.findOne({
+    email: email,
+  });
+  if (!user) {
+    res.status(400).json({
+      message: "user not found",
+    });
+  }
+  if (!email) {
+    res.status(400).json({
+      message: "email is required",
+    });
+  }
+  if (!password) {
+    res.status(400).json({
+      message: "email is required",
+    });
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    res.status(400).json({
+      message: "invalid passoword",
+    });
+  }
+
+  const accessToken = generateAccessToken(user);
+  const refreshToken = generateRefreshToken(user);
+
+  // cookies
+  res.cookie("refreshToken", refreshToken, { http: true, secure: false });
+
+  res.json({
+    message: "user loggedIn successfully",
+    accessToken,
+    refreshToken,
+    data: user,
+  });
+};
+export {
+  encryptPassword,
+  generateAccessToken,
+  generateRefreshToken,
+  registerUser,
+  loginUser,
+};
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,9 +3,13 @@ import {
   encryptPassword,
   generateRefreshToken,
   generateAccessToken,
+  registerUser,
+  loginUser,
 } from "../controllers/user.controllers.js";
 const router = express.Router();
 router.post("/encryptPassword", encryptPassword);
 router.post("/refreshToken", generateRefreshToken);
 router.post("/accessToken", generateAccessToken);
+router.post("/register", registerUser);
+router.post("/loginUser", loginUser);
 export default router;
